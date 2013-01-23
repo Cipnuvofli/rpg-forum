adbffe2a0813b820d2930eb3969acefc7aab326e
Joseph Penrod <Cipnuvofli@gmail.com>
2013-01-22 21:06:15 -0600

    Test commit

---
+++ Register.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<head>
+  <meta charset="utf-8">
+  <title>Register</title>
+  <meta name="description" content="Registration">
+  <link rel="stylesheet" type="text/css" href="CSS/Quest[x].css">
+  <p id = "Logo">QUEST[x].com</p>
+</head>
+
+Honk Honk
---
+++ login.php
@@ -0,0 +1,2 @@
+Hello <?php echo htmlspecialchars($_POST['Username']);?>.
+I am not telling you the password you entered. That would be a security risk.
