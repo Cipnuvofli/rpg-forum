fc3cddd0088db676718064745884616370ff1ecd
stevenwhite91 <stevenwhite91@gmail.com>
2013-02-14 19:22:37 +0000

    Background of the page

--- ForumRules.php
+++ ForumRules.php
@@ -1,2 +1,25 @@
 <!DOCTYPE html>
 
+<head>
+  <meta charset="utf-8">
+  <title>Quest[x].com</title>
+  <meta name="description" content="The Message Board Designed from the ground up for Play by post roleplaying.">
+  <link rel="stylesheet" type="text/css" href="CSS/Quest[x].css">
+
+  <canvas id="Logo" width="320" height="100"></canvas>
+  <script>
+    var canvas = document.getElementById('Logo');
+    var context = canvas.getContext('2d');
+	var x = canvas.width / 2;
+    var y = canvas.height / 2;
+	
+	context.font="2.5em Helvetica";
+	context.textAlign = 'center';
+	context.textBaseline = 'middle';
+	context.fillStyle = "#b19b37"
+	context.fillText("Quest[x].com",x,y);
+
+  </script>
+  
+
+</head>
