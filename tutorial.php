29437751c16eba48c707ac63b2eefe71a6fc3ee9
stevenwhite91 <stevenwhite91@gmail.com>
2013-04-04 12:04:29 -0500

    worked on the tutorial page, added inmages and instructions

--- tutorial.php
+++ tutorial.php
@@ -8,24 +8,32 @@
 </font>
 </div>
 
+<div id="steps">
+<font size="4"><p> Step 1) Log In </p><font size="3"><p>(If you do nto have an account, proceed to "Register")</p></font></font>
+<p><img border="0" style="margin-left:5em" src="Images/firstStep.jpg" alt="First" width="500" height="400"/></p>
+
+<font size="4"><p> Step 2) Enter your information </p></font>
+<font size="4"><p> Answer the simple math problem to prove you are human </p></font>
+<p><img border="0" style="margin-left:5em" src="Images/secondStep.jpg" alt="Second" width="500" height="400"/></p>
+
+<font size="4"><p> Step 3) Read the Forum Rules, agree to them by checking the box & submit </p></font>
+<p><img border="0" style="margin-left:5em" src="Images/thirdStep.jpg" alt="Third" width="500" height="400"/></p>
+
+<font size="4"><p> Step 4) Go back to the home page & Log In </p></font>
+<p><img border="0" style="margin-left:5em" src="Images/firstStep.jpg" alt="First" width="500" height="400"/></p>
+
+<font size="4"><p> Step 5) Once you are logged in, you now have the option to Create a Thread </p></font>
 
 
 
 
+</div>
+</body>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+<div>
+<a href = "Home.php">
+<button style="background-color:#000000;margin-left:42em" type="button";><p>Return Home</p>
+</button>
+</div>
 
 <?php include "footer.php" ?>
