d357a352ad2bc902dab06f73d37dc583a277d45c
stevenwhite91 <stevenwhite91@gmail.com>
2013-04-03 22:55:56 -0500

    added tutorial page, and added button to home page. finishing the tutorial page

---
+++ tutorial.php
@@ -0,0 +1,31 @@
+<?php include "Header.php" ?>
+
+<body>
+
+<div id="tutorial">
+<div id="header" style="color:#b19b37;text-align:center;border:.3em outset #b19b37;margin-left:20em;margin-right:20em;">
+<font size="6">Welcome to the Tutorial
+</font>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include "footer.php" ?>
