b6dd0abf3b4c420f1e077a10a1d9e9b2770614cf
Joseph Penrod <Cipnuvofli@gmail.com>
2013-04-09 12:38:57 -0500

    Commit for mergin?

--- Home.php
+++ Home.php
@@ -1,16 +1,22 @@
+
+
 <?php include "Header.php"?>
 
 <?php include "usercard.php"?>
 
-
-<div id="Buttons">
-<button style="background-color:#000000;margin-left:4.5em" type="button"; onClick="location.href='CreateThread.php'";><p>Create Thread</p>
-</button>
-<button style="background-color:#000000;margin-left:2em"; type="button"; onClick="location.href='tutorial.php'";><p>Tutorial</p>
+<div id = "Buttons">
+<?php
+if(isset($_SESSION['loggedin']))
+{
+echo'<button style="background-color:#00000; margin-left:4.5em" type="button" onClick="location.href=\'CreateThread.php\'">Create Thread';
+echo'</button>';
+}
+?>
+<button style= "background-color:#000000 margin-left:2em" type="button" onClick="location.href='tutorial.php'">Tutorial
 </button>
 </div>
+
 
 <?php include "listtopics.php"?>
 
 <?php include "footer.php"?>
-
