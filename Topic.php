89322991f4785a615db93163dff0b83f8aeba658
Joseph Penrod <Cipnuvofli@gmail.com>
2013-04-17 12:19:32 -0500

    Added TinyMCE rich text editor, working on CSS post styling

--- Topic.php
+++ Topic.php
@@ -20,16 +20,53 @@ $queryid = $_GET['threadid'];
  
 $sql = "SELECT Postid, Timestamp, Content, Poster, Story FROM posts WHERE Threadid = $queryid ";
 
+
+
 $result = mysql_query($sql) or die(mysql_error());
 
-echo '<div id = "posts">';
+
 
 while($row = mysql_fetch_assoc($result))
 {
+	$Postid = $row['Postid'];
+	$Timestamp = $row['Timestamp'];
+	$Content = $row['Content'];
+	$Poster = $row['Poster'];
+	$Story = $row['Story'];
+	
+	
+	echo '<div class = "Post">';
+	echo '<div class = "user">';
+	echo '<p>'.$Poster.'</p>';
+	echo '<div class = "avatar">';
+	echo '<img src = "Images/Avatars/0.jpg">';
+	echo '</div>';
+	echo '</div>';
+	echo '<div class = "content">';
+	echo '<p>'.$Content.'</p>';
+	echo '</div>';
+	echo '</div>';
 	
 }
+if(isset($_SESSION['loggedin']))
+{
+	echo'<script type="text/javascript" src="../jscripts/tiny_mce/tiny_mce.js"></script>';
 
-echo '</div>';
+echo'<script type="text/javascript">';
+echo'tinyMCE.init({';
+       echo 'mode : "textareas"';
+echo '});';
+echo '</script>';
+
+echo'<form method="post" action="insertpost.php">';
+        echo'<p>' ;
+               echo' <textarea name="content" cols="50" rows="15">This is some content that will be editable with TinyMCE.</textarea>';
+                echo'<input type="submit" value="Reply" />';
+        echo'</p>';
+echo'</form>';
+
+
+}
 ?>
 
 <?php include "footer.php"?>
